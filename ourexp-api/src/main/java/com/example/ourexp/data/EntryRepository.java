ec5db733c9d598f39f9d3f6024379a3c2cea7ac3
mads-bohn <mady.bohn@gmail.com>
2024-11-05 20:44:56 -0600

    created entry class and repository, generates mysql table

---
+++ ourexp-api/src/main/java/com/example/ourexp/data/EntryRepository.java
@@ -0,0 +1,11 @@
+package com.example.ourexp.data;
+
+import com.example.ourexp.models.Entry;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface EntryRepository extends CrudRepository<Entry, UUID> {
+}
---
+++ ourexp-api/src/main/java/com/example/ourexp/models/Entry.java
@@ -0,0 +1,35 @@
+package com.example.ourexp.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+import java.util.UUID;
+
+@Entity
+public class Entry {
+
+    @Id
+    @GeneratedValue
+    private UUID id;
+    private String text;
+
+    public Entry(String text) {
+        this.text = text;
+    }
+
+    public Entry() {
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+}
