14dd153b225111d2183e8e4c3f6510af6af9329a
mads-bohn <mady.bohn@gmail.com>
2024-11-07 12:40:30 -0600

    enabled OSIV, created getAllFeelings method

--- ourexp-api/src/main/java/com/example/ourexp/controllers/FeelingController.java
+++ ourexp-api/src/main/java/com/example/ourexp/controllers/FeelingController.java
@@ -3,6 +3,7 @@ package com.example.ourexp.controllers;
 import com.example.ourexp.data.FeelingRepository;
 import com.example.ourexp.models.Feeling;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -27,5 +28,11 @@ public class FeelingController {
         return ResponseEntity.ok(savedFeeling);
     }
 
+    // gets all feelings via GET request to localhost:8080/feeling/getAll
+    @GetMapping("/getAll")
+    public ResponseEntity<List<Feeling>> getAllFeelings() {
+        List<Feeling> feelings = (List<Feeling>) feelingRepository.findAll();
+        return new ResponseEntity<>(feelings, HttpStatusCode.valueOf(200));
+    }
 
 }
