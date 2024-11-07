46edc04d217b6f9555665a51ee157371beb43b00
mads-bohn <mady.bohn@gmail.com>
2024-11-07 13:11:45 -0600

    changed repositories to expect long id type, created getFeelingById method in controller

--- ourexp-api/src/main/java/com/example/ourexp/controllers/FeelingController.java
+++ ourexp-api/src/main/java/com/example/ourexp/controllers/FeelingController.java
@@ -6,12 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/feeling")
@@ -32,7 +30,17 @@ public class FeelingController {
     @GetMapping("/getAll")
     public ResponseEntity<List<Feeling>> getAllFeelings() {
         List<Feeling> feelings = (List<Feeling>) feelingRepository.findAll();
-        return new ResponseEntity<>(feelings, HttpStatusCode.valueOf(200));
+        return ResponseEntity.ok(feelings);
+    }
+
+    // gets feeling by id via GET request to localhost:8080/feeling/id
+    @GetMapping("/{id}")
+    public ResponseEntity getFeelingById(@PathVariable Long id) {
+        Optional<Feeling> feeling = feelingRepository.findById(id);
+        if (feeling.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(feeling);
     }
 
 }
