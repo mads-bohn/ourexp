7f4bdc1ba66f31f2592eab29e3e044c770dde5df
mads-bohn <mady.bohn@gmail.com>
2024-11-11 22:28:51 -0600

    created getEntryById method

--- ourexp-api/src/main/java/com/example/ourexp/controllers/EntryController.java
+++ ourexp-api/src/main/java/com/example/ourexp/controllers/EntryController.java
@@ -2,15 +2,14 @@ package com.example.ourexp.controllers;
 
 import com.example.ourexp.data.EntryRepository;
 import com.example.ourexp.models.Entry;
+import com.example.ourexp.models.Feeling;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/entry")
@@ -31,5 +30,15 @@ public class EntryController {
     public ResponseEntity<List<Entry>> getAllEntries() {
         List<Entry> entries = (List<Entry>) entryRepository.findAll();
         return ResponseEntity.ok(entries);
+    }
+
+    // gets entry by id via GET request to localhost:8080/entry/id
+    @GetMapping("/{id}")
+    public ResponseEntity getEntryById(@PathVariable Long id) {
+        Optional<Entry> entry = entryRepository.findById(id);
+        if (entry.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(entry);
     }
 }
