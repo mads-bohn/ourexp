a91f924e3fa3990dc537be115cd3995d23ddf424
mads-bohn <mady.bohn@gmail.com>
2024-11-11 22:11:09 -0600

    created getAllEntries method, added @JsonIgnore to entries field in Feelings to prevent infinte recursion

--- ourexp-api/src/main/java/com/example/ourexp/controllers/EntryController.java
+++ ourexp-api/src/main/java/com/example/ourexp/controllers/EntryController.java
@@ -5,9 +5,12 @@ import com.example.ourexp.models.Entry;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/entry")
@@ -21,5 +24,12 @@ public class EntryController {
     public ResponseEntity<Entry> addEntry(@RequestBody Entry entry) {
         Entry savedEntry = entryRepository.save(entry);
         return ResponseEntity.ok(savedEntry);
+    }
+
+    // gets all entries via GET request to localhost:8080/entry/getAll
+    @GetMapping("/getAll")
+    public ResponseEntity<List<Entry>> getAllEntries() {
+        List<Entry> entries = (List<Entry>) entryRepository.findAll();
+        return ResponseEntity.ok(entries);
     }
 }
