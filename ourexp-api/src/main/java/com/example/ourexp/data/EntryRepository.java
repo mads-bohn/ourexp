45cf2498ddcc4819d202f9eb4ffd769734b4c575
mads-bohn <mady.bohn@gmail.com>
2024-11-12 22:53:18 -0600

    findByFeelingsContaining query method and corresponding controller

--- ourexp-api/src/main/java/com/example/ourexp/data/EntryRepository.java
+++ ourexp-api/src/main/java/com/example/ourexp/data/EntryRepository.java
@@ -1,11 +1,15 @@
 package com.example.ourexp.data;
 
 import com.example.ourexp.models.Entry;
+import com.example.ourexp.models.Feeling;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.UUID;
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface EntryRepository extends CrudRepository<Entry, Long> {
+
+    List<Entry> findByFeelingsContaining(Optional<Feeling> feeling);
 }
