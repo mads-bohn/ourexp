3c95d8bb1733a602f094a36e5860a7f488ef5a40
mads-bohn <mady.bohn@gmail.com>
2024-11-27 17:05:42 -0600

    boilerplate UserController

---
+++ ourexp-api/src/main/java/com/example/ourexp/controllers/UserController.java
@@ -0,0 +1,20 @@
+package com.example.ourexp.controllers;
+
+import com.example.ourexp.data.UserRepository;
+import com.example.ourexp.models.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+
+}
