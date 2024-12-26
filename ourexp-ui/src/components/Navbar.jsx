2d688c7611c39ca2925f44429ba8bf8154052aec
mads-bohn <mady.bohn@gmail.com>
2024-12-26 14:49:20 -0600

    navbar link hover and active pseudoclasses

--- ourexp-ui/src/components/Navbar.jsx
+++ ourexp-ui/src/components/Navbar.jsx
@@ -6,10 +6,10 @@ export default function Navbar() {
   return (
     <div>
         <ul className='fixed top-0 left-0 w-full z-10 p-6 bg-slate-200'>
-            <Link to="/" className='mx-6'>Our Experience</Link>
-            <Link to="/submit" className='mx-6'>Write</Link>
-            <Link to="/entries" className='mx-6'>Browse</Link>
-            <Link to="/account" className='mx-6'>Account</Link>
+            <Link to="/" className='p-6 hover:bg-slate-300 active:bg-slate-400'>Our Experience</Link>
+            <Link to="/submit" className='p-6 hover:bg-slate-300 active:bg-slate-400'>Write</Link>
+            <Link to="/entries" className='p-6 hover:bg-slate-300 active:bg-slate-400'>Browse</Link>
+            <Link to="/account" className='p-6 hover:bg-slate-300 active:bg-slate-400'>Account</Link>
         </ul>
     </div>
   )
