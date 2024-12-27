3d5351cf1f4910e67fb8a9f0eec811e74abcf338
mads-bohn <mady.bohn@gmail.com>
2024-12-26 18:22:58 -0600

    active page highlights in navbar

--- ourexp-ui/src/components/Navbar.jsx
+++ ourexp-ui/src/components/Navbar.jsx
@@ -7,10 +7,29 @@ export default function Navbar() {
   return (
     <div>
         <ul className='fixed top-0 left-0 w-full z-10 p-6 bg-slate-200'>
-          <NavLink to="/" className='p-6 hover:bg-slate-300 active:bg-slate-400'>Our Experience</NavLink>
-          <NavLink to="/submit" className='p-6 hover:bg-slate-300 active:bg-slate-400'>Write</NavLink>
-          <NavLink to="/entries" className='p-6 hover:bg-slate-300 active:bg-slate-400'>Browse</NavLink>
-          <NavLink to="/account" className='p-6 hover:bg-slate-300 active:bg-slate-400'>Account</NavLink>
+          <NavLink 
+            to="/" 
+            className='p-6 hover:bg-slate-300 active:bg-slate-400'>
+          Our Experience
+          </NavLink>
+          <NavLink 
+            to="/submit" 
+            className={({isActive}) => 
+              isActive ? 'p-6 bg-slate-400 hover:bg-slate-300 active:bg-slate-400' : 'p-6 hover:bg-slate-300 active:bg-slate-400'}>
+            Write
+          </NavLink>
+          <NavLink 
+            to="/entries" 
+            className={({isActive}) => 
+              isActive ? 'p-6 bg-slate-400 hover:bg-slate-300 active:bg-slate-400' : 'p-6 hover:bg-slate-300 active:bg-slate-400'}>
+            Browse
+          </NavLink>
+          <NavLink 
+            to="/account" 
+            className={({isActive}) => 
+              isActive ? 'p-6 bg-slate-400 hover:bg-slate-300 active:bg-slate-400' : 'p-6 hover:bg-slate-300 active:bg-slate-400'}>
+            Account
+          </NavLink>
         </ul>
     </div>
   )
