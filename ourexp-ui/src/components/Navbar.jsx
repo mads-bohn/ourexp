33ecae3619957374007d54db1d41144fd7986bd9
mads-bohn <mady.bohn@gmail.com>
2024-12-26 18:12:08 -0600

    updated navbar links to NavLinks from react router dom

--- ourexp-ui/src/components/Navbar.jsx
+++ ourexp-ui/src/components/Navbar.jsx
@@ -1,15 +1,16 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 import Home from '../pages/Home'
 
 export default function Navbar() {
+
   return (
     <div>
         <ul className='fixed top-0 left-0 w-full z-10 p-6 bg-slate-200'>
-            <Link to="/" className='p-6 hover:bg-slate-300 active:bg-slate-400'>Our Experience</Link>
-            <Link to="/submit" className='p-6 hover:bg-slate-300 active:bg-slate-400'>Write</Link>
-            <Link to="/entries" className='p-6 hover:bg-slate-300 active:bg-slate-400'>Browse</Link>
-            <Link to="/account" className='p-6 hover:bg-slate-300 active:bg-slate-400'>Account</Link>
+          <NavLink to="/" className='p-6 hover:bg-slate-300 active:bg-slate-400'>Our Experience</NavLink>
+          <NavLink to="/submit" className='p-6 hover:bg-slate-300 active:bg-slate-400'>Write</NavLink>
+          <NavLink to="/entries" className='p-6 hover:bg-slate-300 active:bg-slate-400'>Browse</NavLink>
+          <NavLink to="/account" className='p-6 hover:bg-slate-300 active:bg-slate-400'>Account</NavLink>
         </ul>
     </div>
   )
