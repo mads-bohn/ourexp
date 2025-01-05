31eb60508bd7ea9568c4fb2c704694429e7645bd
mads-bohn <mady.bohn@gmail.com>
2025-01-04 22:13:44 -0600

    default loading entry state to prevent errors

--- ourexp-ui/src/pages/Entries.jsx
+++ ourexp-ui/src/pages/Entries.jsx
@@ -6,63 +6,18 @@ import { useParams } from 'react-router-dom'
 import axios from 'axios'
 // import { getEntriesByFeeling } from '../services/getEntriesByFeeling'
 
-const testArray = [
-    {
-        "id": 1,
-        "text": "I think 2025 is going to be a good year for me.",
-        "title": "New Year",
-        "time": "2024-12-24T15:26:02.21123",
-        "feelings": [
-            {
-                "id": 10,
-                "name": "Optimistic",
-                "category": "HAPPY"
-            }
-        ]
-    },
-    {
-        "id": 2,
-        "text": "This journey of getting a tech job has been scary, with a lot of uncertainties. But I know I can count on myself to keep going, and that's what makes me believe it will work out in the end!",
-        "title": "Hope in Tech",
-        "time": "2024-12-24T15:28:15.452663",
-        "feelings": [
-            {
-                "id": 6,
-                "name": "Courageous",
-                "category": "HAPPY"
-            },
-            {
-                "id": 10,
-                "name": "Optimistic",
-                "category": "HAPPY"
-            }
-        ]
-    },
-    {
-        "id": 3,
-        "text": "I've had a feeling that something really good is about to happen to me. I can't wait to find out what it is.",
-        "title": "Feeling lucky",
-        "time": "2024-12-24T15:29:59.886364",
-        "feelings": [
-            {
-                "id": 10,
-                "name": "Optimistic",
-                "category": "HAPPY"
-            },
-            {
-                "id": 21,
-                "name": "Excited",
-                "category": "SURPRISED"
-            }
-        ]
-    }
-]
-
 
 export default function Entries() {
 
     const { feelingId } = useParams();
-    const [entries, setEntries] = useState();
+    const [entries, setEntries] = useState([
+        {
+            "id": 0,
+            "text": "",
+            "title": "Loading...",
+            "time": "",
+            "feelings": []
+        }]);
 
     async function getEntries(id) {
         const result = await axios.get(`http://localhost:8080/entry/feeling/${id}`);
@@ -78,7 +33,7 @@ export default function Entries() {
     return (
         <div className='relative top-8'>
         <Navbar />
-            <DisplayEntries entryArray={testArray} />
+            <DisplayEntries entryArray={entries} />
         </div>
     )
 }
