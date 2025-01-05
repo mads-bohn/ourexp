cb9a5765f0b222f960e620ec2d6169ce83f523c8
mads-bohn <mady.bohn@gmail.com>
2025-01-04 22:21:31 -0600

    error handling in fetchEntries()

--- ourexp-ui/src/pages/Entries.jsx
+++ ourexp-ui/src/pages/Entries.jsx
@@ -19,14 +19,25 @@ export default function Entries() {
             "feelings": []
         }]);
 
-    async function getEntries(id) {
-        const result = await axios.get(`http://localhost:8080/entry/feeling/${id}`);
-        setEntries(result.data);
+    async function fetchEntries(id) {
+        try {
+            const result = await axios.get(`http://localhost:8080/entry/feeling/${id}`);
+            setEntries(result.data);
+        } catch (error) {
+            console.error('Error fetching entries by feeling:', error);
+            setEntries([
+                {
+                    "id": 0,
+                    "text": "",
+                    "title": "Error fetching entries.",
+                    "time": "",
+                    "feelings": []
+                }])
+        }
     }
-    
 
     useEffect(() => {
-        getEntries(feelingId);
+        fetchEntries(feelingId);
     },[]);
 
 
