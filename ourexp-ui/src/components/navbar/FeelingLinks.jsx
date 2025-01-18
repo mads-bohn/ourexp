3932ed937fb480613362ccecb1d05da78012c902
mads-bohn <mady.bohn@gmail.com>
2025-01-18 17:28:15 -0600

    fixed key prop warning

--- ourexp-ui/src/components/navbar/FeelingLinks.jsx
+++ ourexp-ui/src/components/navbar/FeelingLinks.jsx
@@ -3,7 +3,7 @@ import { NavLink } from 'react-router-dom'
 
 export default function FeelingLinks({feelingList}) {
 
-    let links = feelingList.map(feeling => (<li className='text-sm'><NavLink to={"/entries/" + feeling.id}>{feeling.name}</NavLink></li>))
+    let links = feelingList.map(feeling => (<li className='text-sm' key={feeling.id}><NavLink to={"/entries/" + feeling.id}>{feeling.name}</NavLink></li>))
 
   return (
     <ul>{links}</ul>
