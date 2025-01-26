23f835a1d26fc64bd6701412a3fe78d57f6c0f13
mads-bohn <mady.bohn@gmail.com>
2025-01-26 12:47:00 -0600

    confirmation modal with styling

--- ourexp-ui/src/components/entries/DeleteButton.jsx
+++ ourexp-ui/src/components/entries/DeleteButton.jsx
@@ -1,16 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { deleteEntry } from '../../services/deleteEntry'
 
 export default function DeleteButton({id}) {
 
-    const handleClick = async () => {
+    // confirmation modal visibility
+    const [isVisible, setIsVisible] = useState(false);
+
+    const handleDelete = async () => {
         deleteEntry(id);
         // reload page?
     }
 
+    const toggleModal = () => {
+        setIsVisible(!isVisible);
+    }
+
   return (
     <div>
-        <button onClick={handleClick} className='text-red-600 font-bold hover:border-red-600'>Delete</button>
+        <div className={isVisible ? null : 'hidden'}>
+            <div className='fixed top-1/2 left-1/2 ml-[-196px] mt-[-85px] p-8 border-2 border-slate-300 rounded-md bg-white z-50'>
+                <p className='m-2'>Are you sure you want to delete your entry?</p>
+                <button onClick={handleDelete} className='m-2 text-red-600 font-bold hover:border-red-600'>Delete</button>
+                <button onClick={toggleModal} className='m-2'>Cancel</button> 
+            </div>
+        </div>
+        <button onClick={toggleModal} className='text-red-600 font-bold hover:border-red-600'>Delete</button>
     </div>
   )
 }
