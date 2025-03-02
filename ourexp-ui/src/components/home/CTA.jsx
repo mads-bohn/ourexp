4c5feb898434d6ef77608a1faab966163315bb6d
mads-bohn <mady.bohn@gmail.com>
2025-03-01 21:33:30 -0600

    temporary styling

--- ourexp-ui/src/components/home/CTA.jsx
+++ ourexp-ui/src/components/home/CTA.jsx
@@ -4,7 +4,11 @@ import { Link } from 'react-router-dom'
 export default function CTA() {
   return (
     <div>
-        <Link to="/submit">Write an Entry</Link>
+        <Link 
+            to='/submit'
+            className='p-4 bg-indigo-800 text-white rounded-lg hover:text-white hover:bg-indigo-600'>
+                Write an Entry
+        </Link>
     </div>
   )
 }
