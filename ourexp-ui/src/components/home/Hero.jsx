e132a9fe7d0e3b00273821c1c9a091a5c014377f
mads-bohn <mady.bohn@gmail.com>
2025-03-02 09:44:21 -0600

    added unstyled elements to hero

--- ourexp-ui/src/components/home/Hero.jsx
+++ ourexp-ui/src/components/home/Hero.jsx
@@ -4,7 +4,10 @@ import CTA from './CTA'
 export default function Hero() {
   return (
     <div>
-        <p>Hero</p>
+        <img src='src/assets/hero-background.png' alt='Pen and notebook' />
+        <h1>Our Experience.</h1>
+        <h1>Everybody's Journal.</h1>
+        <h2>Share stories and connect with people whose experiences resonate with you.</h2>
         <CTA />
     </div>
   )
