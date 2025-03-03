463d52ca311b739b3aa1bb0b0a07c97320fa2dfb
mads-bohn <mady.bohn@gmail.com>
2025-03-02 19:03:04 -0600

    unstyled instructions component

--- ourexp-ui/src/pages/Account.jsx
+++ ourexp-ui/src/pages/Account.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Navbar from '../components/navbar/Navbar'
+import Instructions from '../components/home/Instructions'
 
 export default function Account() {
   return (
     <div>
         <Navbar />
         <p>Hello, account!</p>
+        <Instructions />
     </div>
   )
 }
