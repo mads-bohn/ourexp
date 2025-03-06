d47b8fcbd47be383db38267cdf412f55d770c2a1
mads-bohn <mady.bohn@gmail.com>
2025-03-05 22:30:40 -0600

    prettier styling

--- ourexp-ui/src/components/home/Card.jsx
+++ ourexp-ui/src/components/home/Card.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Card({title, description, imageSrc}) {
   return (
     <div className='flex-auto p-2 mx-6'>
-      <img src={imageSrc} className='p-4'/>
+      <img src={imageSrc} className='p-6'/>
       <h3 className='my-2 text-2xl'>{title}</h3>
       <p>{description}</p>
     </div>
