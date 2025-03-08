662a66e270b5c1ea379b261bdd8c8f7864bc694b
mads-bohn <mady.bohn@gmail.com>
2025-03-07 18:29:52 -0600

    refactored edit form into edit button

--- ourexp-ui/src/components/entries/EditButton.jsx
+++ ourexp-ui/src/components/entries/EditButton.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
+import ErrorMessage from '../submit/ErrorMessage';
 import { useState } from 'react';
-import EditForm from '../submit/EditForm';
+import { useNavigate } from 'react-router-dom';
 
 export default function EditButton({entry}) {
 
@@ -10,11 +11,38 @@ export default function EditButton({entry}) {
         const toggleModal = () => {
             setIsEditVisible(!isEditVisible);
         }
+
+        const navigate = useNavigate(); // navigation hook
+
+    const [newEntry, setNewEntry] = useState({id: entry.id, title: entry.title, text: entry.text}); // entry object
+    const [errors, setErrors] = useState({}); // errors object
+
+    const handleChange =(e)=> {
+        setNewEntry({...entry,[e.target.name]:e.target.value})
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        const newErrors = validateEntry({title: newEntry.title, text: newEntry.text});
+        setErrors(newErrors);
+    
+        if (Object.keys(newErrors).length === 0) {
+          editEntry(entry.id, newEntry.title, newEntry.text, entry.feelings);
+          return navigate(`/entries/${entry.feelings[0].id}`);
+        }
+      }
   
     return (
         <div>
             <div className={isEditVisible ? null : 'hidden'}>
-                <EditForm entry={entry} />
+                <div className='divide-y-2 border-solid border-2 border-slate-200'>
+                    <input type='text' id='title' name='title' value={newEntry.title} size={75} onChange={(e)=>handleChange(e)}/>
+                    <ErrorMessage message={errors.title} />
+                    <textarea id='text' name='text' rows={9} cols={75} value={newEntry.text} onChange={(e)=>handleChange(e)}></textarea>
+                    <ErrorMessage message={errors.text} />
+                </div>
+                <br />
+                <button className='bg-indigo-800 text-white my-4 inline' onClick={handleSubmit}>Submit</button>
                 <button onClick={toggleModal} className='m-2 inline'>Cancel</button> 
             </div>
             <button onClick={toggleModal} className='text-slate-600 font-bold hover:border-slate-600'>Edit</button>
