a482c19e16faa9db0b3d18135a7d8d162f6861ea
mads-bohn <mady.bohn@gmail.com>
2025-03-08 23:38:06 -0600

    reversed entry display order and set max width

--- ourexp-ui/src/components/entries/Entry.jsx
+++ ourexp-ui/src/components/entries/Entry.jsx
@@ -4,7 +4,7 @@ import OptionsDropdown from './OptionsDropdown'
 
 export default function Entry({entryObject}) {
   return (
-    <div className=' my-6 mx-40 p-4 border-2'>
+    <div className='max-w-2xl my-6 mx-40 p-4 border-2'>
         <div className='flex'>
             <h3 className='flex-auto text-xl text-left'>{entryObject.title}</h3>
             <h4 className='flex-auto text-right'>{entryObject.time.slice(0, 10)}</h4>
