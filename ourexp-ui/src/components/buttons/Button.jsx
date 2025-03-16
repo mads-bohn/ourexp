1a14ef6524fa3ce1dc95a67e9ba400387303b8b0
mads-bohn <mady.bohn@gmail.com>
2025-03-16 13:55:01 -0500

    removed size prop

--- ourexp-ui/src/components/buttons/Button.jsx
+++ ourexp-ui/src/components/buttons/Button.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-export default function Button({title, size, onClick}) {
+export default function Button({title, onClick}) {
   return (
     <button 
-      className={`bg-lime-700 text-white text-${size} rounded-lg`}
+      className={`bg-lime-700 text-white rounded-lg`}
       onClick={onClick}
     >
       {title}
