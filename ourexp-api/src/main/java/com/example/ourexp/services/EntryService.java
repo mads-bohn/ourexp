52495c77cc5b782c1f06c4a35c02a5701864a21d
mads-bohn <mady.bohn@gmail.com>
2025-03-20 15:19:50 -0500

    containsKeywords service

--- ourexp-api/src/main/java/com/example/ourexp/services/EntryService.java
+++ ourexp-api/src/main/java/com/example/ourexp/services/EntryService.java
@@ -1,9 +1,21 @@
 package com.example.ourexp.services;
 
+import com.example.ourexp.models.Entry;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class EntryService {
 
+    // returns true if entry title or text contains any of given keywords
+    public Boolean containsKeywords(Entry entry, List<String> keywords) {
+        for (String keyword : keywords) {
+            if (entry.getTitle().contains(keyword) || entry.getText().contains(keyword)) {
+                return true;
+            }
+        }
+        return false;
+    }
     
 }
