46d3b5c9344118c2b49061b942bb6e4e484c21db
mads-bohn <mady.bohn@gmail.com>
2025-05-25 18:04:22 -0500

    login button styling

--- ourexp-ui/src/components/navbar/LoginButton.jsx
+++ ourexp-ui/src/components/navbar/LoginButton.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 
 export default function LoginButton() {
   return (
-    <div><a href='http://localhost:8080/oauth2/authorization/google'>Sign in with Google</a></div>
+    <div className='inline p-6 hover:bg-slate-300 active:bg-slate-400'>
+      <a href='http://localhost:8080/oauth2/authorization/google'>Sign in with Google</a>
+    </div>
   )
 }
